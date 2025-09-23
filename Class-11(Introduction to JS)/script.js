1ab6ecd7260ce1694f5b7cee127d9fd5fc771345
mrinal1224 <iammrinalkc@gmail.com>
2025-09-23 12:14:43 +0530

    notes pushed

---
+++ Class-11(Introduction to JS)/script.js
@@ -0,0 +1,27 @@
+console.log('hello from Browser')
+
+// Variables
+ var a; // undefined
+ var b = 1.2
+ var c = 200000000 // Numbers
+
+ var d= 'hello' // String
+
+  var d= `hello ${b}` // Template Literals
+
+ var e = true // Boolean
+
+ var f = null // null
+
+
+console.log(a)
+console.log(d)
+console.log(e)
+
+
+// Primitive DataTypes
+//undefined
+// Number
+// Strings
+// boolean
+//null 
---
+++ Class-11(Introduction to JS)/variables.js
@@ -0,0 +1,27 @@
+var a = 'hello'
+
+var a = 12
+
+a = 16
+
+
+
+// Redeclaration is bad
+// Reassignment
+
+// let and const
+ let b ='Hello from let'
+
+//  let b= 30 // Redecalartion is not allowed
+
+ b = 40 // Reassigning is allowed
+console.log(b)
+
+ // const
+
+ const c = 20
+ console.log(c)
+
+
+
+
