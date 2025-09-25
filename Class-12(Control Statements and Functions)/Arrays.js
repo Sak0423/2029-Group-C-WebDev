dbbbb2ff38b0ddb9930774a31c0e7808a1e68114
mrinal1224 <iammrinalkc@gmail.com>
2025-09-25 12:40:51 +0530

    arrays intro

---
+++ Class-12(Control Statements and Functions)/Arrays.js
@@ -0,0 +1,44 @@
+// Arrays
+let arr = [1 ,2 ,3,4 ,5 , 'scaler' , true , null , undefined]
+
+
+
+let cars=['BMW' , "Pajero" , "Alto"]
+
+
+// access an element
+
+// console.log(cars[1])
+
+// modify the array - basic Array Methods
+
+// If you want to add some value at the end of an Array
+
+// push
+// cars.push('Nano')
+
+// pop - remove element from the end
+
+// const pop1 = cars.pop()
+// console.log(pop1)
+// // cars.pop()
+// // cars.pop()
+// console.log(cars)
+
+// shift , unshift
+
+console.log(cars)
+
+// shift
+
+cars.shift()
+console.log(cars)
+
+// unShift
+
+cars.unshift('Mercedes')
+console.log(cars)
+
+
+
+
