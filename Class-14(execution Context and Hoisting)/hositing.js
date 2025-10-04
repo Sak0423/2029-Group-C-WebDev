595acb48ded4f771c1f6c4b8b8f47642effffb64
mrinal1224 <iammrinalkc@gmail.com>
2025-10-04 11:51:14 +0530

    Execution context and Hoisting

---
+++ Class-14(execution Context and Hoisting)/hositing.js
@@ -0,0 +1,26 @@
+// ES6  , let and const 
+
+// TDZ - temporal Dead Zone
+
+
+// Hoisting
+
+const a = 20
+console.log(a) 
+
+
+
+// function sayHello(){
+//     console.log('hello')
+// }
+
+// function as expression
+
+var sayHello = function(){
+    console.log('Hello')
+}
+
+sayHello() // executes - function 
+
+
+
